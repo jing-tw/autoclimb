74ee684c4109f244a251019bcaa266cf3a4f5628
Jing <mqjing@gmail.com>
2019-03-29 17:28:34 +0800

    [add] qt gui

--- main.py
+++ main.py
@@ -24,24 +24,65 @@ def read_member_list(strFile):
     return list_person
 
 
+
+
+
+
+
+
+
+import sys
+import random
+from PySide2 import QtCore, QtWidgets, QtGui
+class MyWidget(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.hello = ["Hallo Welt", "Hei maailma", "Hola Mundo", "Привет мир"]
+
+        self.button = QtWidgets.QPushButton("Click me!")
+        self.text = QtWidgets.QLabel("Hello World")
+        self.text.setAlignment(QtCore.Qt.AlignCenter)
+
+        self.layout = QtWidgets.QVBoxLayout()
+        self.layout.addWidget(self.text)
+        self.layout.addWidget(self.button)
+        self.setLayout(self.layout)
+
+        self.button.clicked.connect(self.magic)
+
+
+    def magic(self):
+        self.text.setText(random.choice(self.hello))
+
+
+
+
+
+
 def init_arg():
     parser = argparse.ArgumentParser()
+    parser.add_argument('-gui', '--gui', type=int, default = 1, help="Enable GUI mode")
     parser.add_argument('-p', '--park', type=int, default = 2, help="National Park (0: YUSHAN NATIONAL PARK, 1: TAROKO NATIONAL PARK, 2: SHEI-PA NATIONAL PARK")
     parser.add_argument('-list', '--memberlist', default = 'sample.xlsx', help='sample.xlsx')
 
     args = parser.parse_args()
     #print("args.park =", args.park)
-    return 1, {'park':args.park, 'memberlist':args.memberlist}
-
+    return 1, {'park':args.park, 'memberlist':args.memberlist, 'gui': args.gui}
 
 def main():
     bValid, dict_arg = init_arg()
 
-    lst_mem = read_member_list(dict_arg['memberlist'])
-
-    obj_auto = ParkAuto(dict_arg['park'], lst_mem)
-    obj_auto.run()
-    
+    if dict_arg['gui'] == 0:
+        lst_mem = read_member_list(dict_arg['memberlist'])
+        obj_auto = ParkAuto(dict_arg['park'], lst_mem)
+        obj_auto.run()
+    else:
+        app = QtWidgets.QApplication([])
+        widget = MyWidget()
+        widget.resize(800, 600)
+        widget.show()
+        sys.exit(app.exec_())
 
 if __name__ == '__main__':
     main()
